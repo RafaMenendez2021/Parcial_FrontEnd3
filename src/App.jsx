b4eccc17f4cc3d554961b0cd1ff7f8e8cd3d061f
Rafael Porras <rflporrasmen@gmail.com>
2024-08-13 19:54:22 -0600

    Primer commit para subir mi proyecto a github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <>
+    <h1>Primer parcial Front end 3</h1>
+    <form></form>
+    <Card />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Hola, esto es una tarjeta de prueba xd</div>
+  )
+}
+
+export default Card
